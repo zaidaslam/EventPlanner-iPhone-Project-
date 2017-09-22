aa0aa6b207a0d515c9318dc73544cb356a2ad6ab
Zaid Aslam Shaikh <s3590683@w8408183.cs.rmit.edu.au>
2017-09-22 10:25:15 +1000

    Initial Commit

---
+++ EventPlanner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EventPlanner
+//
+//  Created by Zaid Aslam Shaikh on 9/22/17.
+//  Copyright © 2017 RMIT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
